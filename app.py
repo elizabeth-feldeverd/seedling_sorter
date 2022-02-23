b349a40c78f4fdb03c7c62eac154b325a9b79b6d
Elizabeth Oda <efeldeverd@gmail.com>
2022-02-23 21:09:27 +0900

    import statement

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from itertools import cycle
 import streamlit as st
 import numpy as np
 from PIL import Image
-from skimage import transform
+from scikit-image import transform
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
 
